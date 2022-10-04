b0ba9ab4f04df46eaf8bf90d35b027a954555a3b
SaurabhGuptaGoldy <114756351+SaurabhGuptaGoldy@users.noreply.github.com>
2022-10-04 22:31:57 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int n1 = scan.nextInt();
+        int n2 = scan.nextInt();
+        scan.close();
+
+        System.out.println(a);
+        System.out.println(n1);
+        System.out.println(n2);
+    }
+}
